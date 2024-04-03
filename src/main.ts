d24e468130a2a1f7a7985da26f9da39eb9850df6
mrrobot <mattdemichele@gmail.com>
2024-04-03 17:45:24 -0400

    progress?

--- src/main.ts
+++ src/main.ts
@@ -5,21 +5,21 @@
 import { Octokit } from '@octokit/core'
 import { restEndpointMethods } from '@octokit/plugin-rest-endpoint-methods'
 import * as core from '@actions/core'
-import { execSync } from 'child_process'
+// import { execSync } from 'child_process'
 // import { createActionAuth } from '../node_modules/@octokit/auth-action'
 
-async function findOpenPRs(octokit, commitSHA) {
-  const { data: issues } = await octokit.search.issuesAndPullRequests({
-    q: `is:open is:pr ${commitSHA}`
-  })
+// async function findOpenPRs(octokit, commitSHA) {
+//   const { data: issues } = await octokit.search.issuesAndPullRequests({
+//     q: `is:open is:pr ${commitSHA}`
+//   })
 
-  return issues.items.filter(issue => issue.pull_request)
-}
+//   return issues.items.filter(issue => issue.pull_request)
+// }
 
-async function getTargetBranch(octokit, prURL) {
-  const pr = await octokit.request(prURL)
-  return pr.data.base.ref
-}
+// async function getTargetBranch(octokit, prURL) {
+//   const pr = await octokit.request(prURL)
+//   return pr.data.base.ref
+// }
 
 export async function main() {
   console.log('are we in main?')
@@ -36,49 +36,91 @@ export async function main() {
       // baseUrl: process.env.GITHUB_API_URL
     })
 
+    const ownerAndRepo = process.env.GITHUB_REPOSITORY?.split('/')
+    const owner = ownerAndRepo[0]
+    const repo = ownerAndRepo[1]
+
     const pull_number = process.env.GITHUB_REF_NAME?.split('/')?.[0]
     // get current PR
     console.log('pull_number', pull_number)
 
-    const ownerAndRepo = process.env.GITHUB_REPOSITORY?.split('/')
-
     const currentPR = await octokit.rest.pulls.get({
-      owner: ownerAndRepo[0],
-      repo: ownerAndRepo[1],
+      owner,
+      repo,
       pull_number
     })
 
     console.log('currentPR', currentPR)
 
-    const commitSHA = process.env.GITHUB_SHA
+    const descendantPRs = []
+    let nextPR = currentPR
 
-    const openPRs = await findOpenPRs(octokit, commitSHA)
-    if (openPRs.length === 0) {
-      core.info('No open PRs found.')
-      return
+    // TODO: can we find trunk branch from envars?
+    while (nextPR.data.base.ref !== 'develop') {
+      const prList = await octokit.rest.pulls.list({
+        owner,
+        repo,
+        head: nextPR.data.base
+      })
+
+      console.log('prList', prList)
+
+      if (prList.data.length !== 1) {
+        throw new Error(
+          `The chain of PRs is broken because we could not find a PR with the specified base ${nextPR.data.base} or we found more than one`
+        )
+      }
+
+      const pr = prList.data[0]
+
+      const commits = await octokit.rest.pulls.listCommits({
+        owner,
+        repo,
+        pull_number: pr.number
+      })
+
+      console.log('commits', commits)
+
+      if (commits.data.length > 1) {
+        throw new Error(`PR #${pr.number} has more than one commit`)
+      }
+
+      descendantPRs.push(pr)
+
+      nextPR = pr
     }
 
-    const pr = openPRs[0]
-    const targetBranch = await getTargetBranch(octokit, pr.pull_request.url)
+    console.log('somehow we got out?', descendantPRs)
 
-    console.log('targetBranch', targetBranch)
+    // const commitSHA = process.env.GITHUB_SHA
 
-    if (targetBranch === 'develop') {
-      core.info("Final PR found targeting 'develop'. Rebase and merge...")
-      execSync(`git fetch origin ${pr.head.ref}`)
-      execSync(`git checkout -b pr-${pr.number}-branch FETCH_HEAD`)
-      execSync(`git rebase origin/develop`)
-      execSync(`git push origin pr-${pr.number}-branch:refs/heads/develop`)
-      execSync(
-        `gh pr merge --squash --delete-branch --auto -m "Rebased and merged PR into develop" ${pr.number}`
-      )
-    } else {
-      core.info("Target branch is not 'develop'. Merging...")
-      execSync(`git fetch origin ${pr.head.ref}`)
-      execSync(`git checkout -b pr-${pr.number}-branch FETCH_HEAD`)
-      execSync(`git merge --no-ff --no-edit ${commitSHA}`)
-      execSync(`git push origin pr-${pr.number}-branch:${targetBranch}`)
-    }
+    // const openPRs = await findOpenPRs(octokit, commitSHA)
+    // if (openPRs.length === 0) {
+    //   core.info('No open PRs found.')
+    //   return
+    // }
+
+    // const pr = openPRs[0]
+    // const targetBranch = await getTargetBranch(octokit, pr.pull_request.url)
+
+    // console.log('targetBranch', targetBranch)
+
+    // if (targetBranch === 'develop') {
+    //   core.info("Final PR found targeting 'develop'. Rebase and merge...")
+    //   execSync(`git fetch origin ${pr.head.ref}`)
+    //   execSync(`git checkout -b pr-${pr.number}-branch FETCH_HEAD`)
+    //   execSync(`git rebase origin/develop`)
+    //   execSync(`git push origin pr-${pr.number}-branch:refs/heads/develop`)
+    //   execSync(
+    //     `gh pr merge --squash --delete-branch --auto -m "Rebased and merged PR into develop" ${pr.number}`
+    //   )
+    // } else {
+    //   core.info("Target branch is not 'develop'. Merging...")
+    //   execSync(`git fetch origin ${pr.head.ref}`)
+    //   execSync(`git checkout -b pr-${pr.number}-branch FETCH_HEAD`)
+    //   execSync(`git merge --no-ff --no-edit ${commitSHA}`)
+    //   execSync(`git push origin pr-${pr.number}-branch:${targetBranch}`)
+    // }
   } catch (error) {
     core.setFailed(error.message)
   }
